221a1bf9cc1ef67623f99ee90e1fb95567a327d7
AdnanMahida <adnanmahida1@yahoo.com>
2022-02-05 01:08:01 +0530

    4 feb

--- app.py
+++ app.py
@@ -19,11 +19,73 @@ mysql = MySQL(app)
 #templates rendering
 @app.route('/')
 def index():
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute(" SELECT  * FROM blood_request_list")
-    request_list = cursor.fetchall()
-    return render_template('index.html',request_list=request_list)
+    request_list_cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    blood_group_cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    blood_stock_cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    my_list_cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
+    blood_group_cursor.execute("SELECT * FROM blood_group")
+    blood_group=blood_group_cursor.fetchall()
+
+    request_list_cursor.execute("SELECT * FROM blood_request_list")
+    request_list = request_list_cursor.fetchall()
+    
+    blood_stock_cursor.execute("SELECT * FROM blood_stock,blood_group where blood_stock.blood_stock_id = blood_group.blood_group_id")
+    blood_stock=blood_stock_cursor.fetchall()
+
+    if "loggedin" in session:
+        user_id = session['id']
+        request_list_query = """
+                SELECT 
+                    blood_request.blood_request_id AS blood_request_id,
+                    blood_group.blood_group_id AS blood_group_id,
+                    blood_group.blood_group_name AS blood_group_name,
+                    users.full_name AS full_name,
+                    users.city AS city,
+                    users.contact AS contact,
+                    blood_request.blood_qty_ml AS blood_qty_ml,
+                    blood_request.is_approved AS is_approved,
+                    blood_request.timestamp AS timestamp
+                FROM
+                    ((blood_group
+                    JOIN blood_request)
+                    JOIN users)
+                WHERE
+                    ((blood_request.blood_group_id = blood_group.blood_group_id)
+                    AND (blood_request.user_id = users.user_id))
+                    AND users.user_id <> %s
+                ORDER BY blood_request.blood_request_id
+        """
+
+        request_list_cursor.execute(request_list_query,[user_id])
+        request_list = request_list_cursor.fetchall()
+
+        query="""
+                SELECT 
+                    blood_request.blood_request_id AS blood_request_id,
+                    blood_group.blood_group_id AS blood_group_id,
+                    blood_group.blood_group_name AS blood_group_name,
+                    users.full_name AS full_name,
+                    users.city AS city,
+                    users.contact AS contact,
+                    blood_request.blood_qty_ml AS blood_qty_ml,
+                    blood_request.is_approved AS is_approved,
+                    blood_request.timestamp AS timestamp
+                FROM
+                    ((blood_group
+                    JOIN blood_request)
+                    JOIN users)
+                WHERE
+                    ((blood_request.blood_group_id = blood_group.blood_group_id)
+                        AND (blood_request.user_id = users.user_id))
+                        AND users.user_id = %s
+                ORDER BY blood_request.blood_request_id
+            """
+        
+        my_list_cursor.execute(query,[user_id])
+        my_request_list=my_list_cursor.fetchall()
+        return render_template('index.html',my_request_list = my_request_list,request_list=request_list,blood_group=blood_group,blood_stock=blood_stock)
+    return render_template('index.html',blood_stock=blood_stock,request_list=request_list)
 @app.route('/about')
 def about():
     return render_template('index.html')
@@ -51,21 +113,20 @@ def profile():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        username = request.form['email']
+        email = request.form['email']
         password = request.form['password']
 
+        print('Login successful')
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(
-            'SELECT * FROM users WHERE full_name = %s AND password = %s', (username, password))
+            'SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
         user = cursor.fetchone()
 
         if user:
             session['loggedin'] = True
             session['id'] = user['user_id']
-            session['username'] = user['full_name']
+            session['email'] = user['email']
             return redirect(url_for('index'))
-        else:
-            flash("Incorrect username/password!", "danger")
     return render_template('login.html', title="Login")
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -124,16 +185,39 @@ def user():
         content = {}
         if mysql:
             return "success"
-        # for result in rows:
-        #     content = {'username': result[0], 'email': result[1], 'password': result[2]
-        #                ,'create_time':result[3]}
-        #     payload.append(content)
-        #     content = {}
-        # return payload
-        
-    
+
     except Exception as e:
         return {'error': str(e)}
+
+
+@app.route('/insert-donation',methods=['GET', 'POST'])
+def insert_dontation():
+    if request.method == 'POST' and 'fullname' in request.form:
+        fullname = request.form['fullname']
+        contact  = request.form['contact']
+        city  = request.form['city']
+        blood_group  = request.form['blood_group_id']
+        blood_qty  = request.form['blood_qty']
+        user_id = session['id']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        query ='INSERT INTO blood_request VALUES (NULL,%s,%s,%s,0,NULL,CURRENT_TIMESTAMP)'
+        t  = (user_id, blood_group, blood_qty,)
+        cursor.execute(query,t)
+        mysql.connection.commit()
+        flash("You have successfully applied!", "success")
+    return redirect(url_for('index'))
+
+@app.route('/update-request/<int:id>',methods=['GET', 'POST'])
+def update_request(id):
+    if request.view_args['id']:
+        user_id = session['id']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        query ='UPDATE blood_request SET is_approved = 1, donor_id = %s WHERE blood_request_id = %s'
+        t  = (user_id,id)
+        cursor.execute(query,t)
+        mysql.connection.commit()
+        flash("For your blood donation, whenever you get free you can visite our B.R.C. hospital and donate the blood !", "success")
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
